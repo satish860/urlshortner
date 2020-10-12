e1a17b942edef749cae1268a1235130a0f49d685
satish venkatakrishnan <svenkatakris@vmware.com>
2020-10-12 18:42:23 +0530

    Initial API for URL Shortner

---
+++ src/Urlshortner.API/Controllers/UrlshortnerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Urlshortner.core;
+
+namespace Urlshortner.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UrlshortnerController : ControllerBase
+    {
+        public IActionResult CreateShortUrl(string url)
+        {
+            return Ok("");
+        }
+    }
+}
